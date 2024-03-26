fc38d1134a2051a62674330d3ad9d5d3e5ccc6f7
Chigo <72313512+coolchigi@users.noreply.github.com>
2024-03-26 21:30:52 +0000

    Create Navigation sidebar for application #1

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,71 @@
-const SideBar = () =>{
+import Image from "next/image";
+import Link from "next/link";
+import { GrHomeRounded } from "react-icons/gr";
+import { SiGoogleanalytics } from "react-icons/si";
+import { PiWallet } from "react-icons/pi";
+import { CiSettings } from "react-icons/ci";
+import { FaRegUserCircle } from "react-icons/fa";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
 
-    return <div>SideBar</div>;
+import { useRouter, usePathname  } from 'next/navigation'
+import { useAuth } from "@pangeacyber/react-auth";
+import { useEffect, useState } from 'react';
+
+
+export default function SideBar() {
+    const {user, authenticated} = useAuth();
+
+    const [sidebarItems, setSidebarItems] = useState([
+        { name: "Dashboard", href: "/", icon: <GrHomeRounded /> },
+        { name: "Transactions", href: "/transactions", icon: <RiMoneyDollarCircleLine /> },
+        { name: "Analytics", href: "/analytics", icon: <SiGoogleanalytics /> },
+        { name: "Budgets", href: "/budgets", icon: <PiWallet /> },
+        { name: "Settings", href: "/settings", icon: <CiSettings /> },
+    ]);
+
+
+    useEffect(() => {
+        if (authenticated) {
+            setSidebarItems(prevItems => [
+                ...prevItems,
+                { name: `User (${user.profile.first_name})`, href: "/user", icon: <FaRegUserCircle /> }
+            ]);
+        }
+    }, [authenticated]);
+    const pathname = usePathname();
+    return (<div>
+        <aside className="sidebar">
+            <div className="sidebar__top">
+                <Image src="/Designer.png" 
+                width={80} 
+                height={80} 
+                className="sidebar__logo"
+                alt="Logo"
+                />
+            </div>
+            <p className="sidebar__logo-name">Finch</p>
+            <ul className="sidebar__list">
+            {sidebarItems.map(({ name, href, icon }) => {
+            return (
+              <li className="sidebar__item" key={name}>
+                <Link
+                  className={`sidebar__link ${
+                    pathname === href ? "sidebar__link--active" : ""
+                  }`}
+                  href={href}
+                >
+                  <span className="sidebar__icon">
+                    {icon}
+                  </span>
+                  <span className="sidebar__name">{name}</span>
+                </Link>
+              </li>
+            );
+          })}
+            </ul>
+        </aside>
+    </div>
+    );
 };
 
 
-export default SideBar;
