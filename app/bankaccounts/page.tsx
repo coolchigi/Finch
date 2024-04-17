b3c1f7a309fa9c9679dccc059b44ebd326ebf47f
Chigo <72313512+coolchigi@users.noreply.github.com>
2024-04-17 22:06:12 +0000

    Clean up code #2

--- app/bankaccounts/page.tsx
+++ app/bankaccounts/page.tsx
@@ -4,6 +4,16 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 const BankAccounts = () => {
 
+    return (
+        <Card>
+        <CardHeader>
+            <CardTitle>Bank Accounts</CardTitle>
+        </CardHeader>
+        <CardContent>
+            <p>Bank accounts will be displayed here.</p>
+        </CardContent>
+        </Card>
+    );
 };
 
 export default BankAccounts;
